3ae0aa101223cd04e1f3150396b08350ba5cc081
jirabedra <juaniirabedra@gmail.com>
2023-10-05 22:34:53 -0300

    completed assignment

---
+++ ReflectionQuest/ReflectionQuest/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReflectionQuest;
+using System.Reflection;
+ReflectionHelper reflectionHelper = new ReflectionHelper();
+Console.WriteLine("What's your team number?");
+bool ok = false;
+int number = 0;
+while (!ok)
+{
+    try
+    {
+        number = Int32.Parse(Console.ReadLine());
+        ok = true;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("That's not your team number :( ");
+    }
+}
+switch (number)
+{
+    case 1:
+        reflectionHelper.GetNonStaticPublicClassMemberMethodWithLargestArgumentList(@"../../../../ClassLibrary1");
+        break;
+    case 2:
+        reflectionHelper.FindMethodWithLocalVariablesOfTypeIntAndBool(@"../../../../ClassLibrary2");
+        break;
+    case 3:
+        reflectionHelper.GetTypeThatImplementsIEnumerable(@"../../../../ClassLibrary1");
+        break;
+    case 4:
+        break;
+        reflectionHelper.GetTypeThatHasNestedTypeInSpanish(@"../../../../ClassLibrary2");
+}
+
+
---
+++ ReflectionQuest/ReflectionQuest/ReflectionHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionQuest
+{
+    internal class ReflectionHelper
+    {
+        public MemberInfo GetNonStaticPublicClassMemberMethodWithLargestArgumentList(string assemblyPath)
+        {
+            Console.WriteLine("Get the name of a class member which is non static nad has the largest argument list in assembly");
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            Console.ReadLine();
+            throw new NotImplementedException("Not implemented D:");
+        }
+
+        public MemberInfo FindMethodWithLocalVariablesOfTypeIntAndBool(string assemblyPath)
+        {
+            Console.WriteLine("Get the name of a method with local variables of type bool and int");
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            Console.ReadLine();
+            throw new NotImplementedException("Not implemented D:");
+        }
+
+        public MemberInfo GetTypeThatImplementsIEnumerable(string assemblyPath)
+        {
+            Console.WriteLine("Get the name of a type that implements IEnumerable");
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            Console.ReadLine();
+            throw new NotImplementedException("Not implemented D:");
+        }
+
+        public MemberInfo GetTypeThatHasNestedTypeInSpanish(string assemblyPath)
+        {
+            Console.WriteLine("Get the name of a class member which has a nested type in spanish");
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            Console.ReadLine();
+            throw new NotImplementedException("Not implemented D:");
+        }
+    }
+}
